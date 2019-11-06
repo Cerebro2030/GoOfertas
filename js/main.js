20a68712ece459f82c2ed0bb0e2606142d9fa978
Cerebro2030 <57446351+Cerebro2030@users.noreply.github.com>
2019-11-06 11:48:51 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,631 @@
+const contactosIdentidades = [
+  {
+    nombre: "facebook",
+    identidadURL: "img/FaceNaranja.svg"
+  },
+  {
+    nombre: "instagram",
+    identidadURL: "img/InstaNaranja.svg"
+  },
+  {
+    nombre: "whatsapp",
+    identidadURL: "img/WhatsappNaranja.svg"
+  }
+];
+
+const comercios = [
+  {
+    nombre: "Expomotos",
+    identidadURL: "img/ComerciosIdentidades/comercio1.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/169874710316877/",
+        urlAndroid: "fb://page/169874710316877",
+        urlIOS: "fb://profile/169874710316877"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/expomotos_/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+    ]
+  },
+  {
+    nombre: "MGBikeshop",
+    identidadURL: "img/ComerciosIdentidades/comercio2.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/156576654538911/",
+        urlAndroid: "fb://page/156576654538911",
+        urlIOS: "fb://profile/156576654538911"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/mg_bikeshop"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+    ]
+  },
+  {
+    nombre: "NoEnd",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/1489341201149199/",
+        urlAndroid: "fb://page/1489341201149199",
+        urlIOS: "fb://profile/1489341201149199"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/noendvm"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+    ]
+  },
+  {
+    nombre: "Chernobyl",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/817144355073637/",
+        urlAndroid: "fb://page/817144355073637",
+        urlIOS: "fb://profile/817144355073637"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/chernobyl_vm"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+    ]
+  },
+  {
+    nombre: "LadyRowena",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/229401460777143/",
+        urlAndroid: "fb://page/229401460777143",
+        urlIOS: "fb://profile/229401460777143"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/ladyrowena9947"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "SanTelmo",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/249252289093002/",
+        urlAndroid: "fb://page/249252289093002",
+        urlIOS: "fb://profile/249252289093002"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/santelmovm"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "Cannelle",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: " ",
+        urlAndroid: " ",
+        urlIOS: " "
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/cannelle.vm/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "battiano",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: " ",
+        urlAndroid: " ",
+        urlIOS: " "
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/battiano.vm/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "ElPuesto",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/492856491049255/",
+        urlAndroid: "fb://page/492856491049255",
+        urlIOS: "fb://profile/492856491049255"
+      },
+      {
+        nombre: "instagram",
+        url: "https://instagram.com/elpuestovm"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "LeóndePiedraProducciones",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/1348193145308047/",
+        urlAndroid: "fb://page/1348193145308047",
+        urlIOS: "fb://profile/1348193145308047"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/leondepiedraprod/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "Morrison",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/1498169413809949/",
+        urlAndroid: "fb://page/1498169413809949",
+        urlIOS: "fb://profile/1498169413809949"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/morrisonbarvm/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "ElViejo",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/200641159975597/",
+        urlAndroid: "fb://page/200641159975597",
+        urlIOS: "fb://profile/200641159975597"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/elviejovm/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "SiriusViajes",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/446408782840672/",
+        urlAndroid: "fb://page/446408782840672",
+        urlIOS: "fb://profile/446408782840672"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/sirius.viajes.vm/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "MaxiSport",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/1145527585490062/",
+        urlAndroid: "fb://page/1145527585490062",
+        urlIOS: "fb://profile/1145527585490062"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/maxisportgimnasio/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "Moldear",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/2220787721539920/",
+        urlAndroid: "fb://page/2220787721539920",
+        urlIOS: "fb://profile/2220787721539920"
+      },
+      {
+        nombre: "instagram",
+        url: " "
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "Instintos",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/193103137937804/",
+        urlAndroid: "fb://page/193103137937804",
+        urlIOS: "fb://profile/193103137937804"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/instintosgiunta/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "Vocaturo",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/815298375289037/",
+        urlAndroid: "fb://page/815298375289037",
+        urlIOS: "fb://profile/815298375289037"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/casavocaturo/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "ElEstampadero",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/491951830973493/",
+        urlAndroid: "fb://page/491951830973493",
+        urlIOS: "fb://profile/491951830973493"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/el_estampadero/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "ICR",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/892937807483107/",
+        urlAndroid: "fb://page/892937807483107",
+        urlIOS: "fb://profile/892937807483107"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/icr.villamercedes/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "SIMAlarmas",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/2151566668403178/",
+        urlAndroid: "fb://page/2151566668403178",
+        urlIOS: "fb://profile/2151566668403178"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/sim.seguridad.vm/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "AguaMania",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/2343757439024528/",
+        urlAndroid: "fb://page/2343757439024528",
+        urlIOS: "fb://profile/2343757439024528"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/agua.mania.vm/"
+      },
+      {
+        nombre: "whatsapp",
+        url: " "
+      }
+          
+    ]
+  },
+  {
+    nombre: "Manilla",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+
+      {
+        nombre: "whatsapp",
+        url: "https://wa.me/542657469160"
+      }
+          
+    ]
+  },
+  {
+    nombre: "AlToke",
+    identidadURL: "img/ComerciosIdentidades/comercio3.svg",
+    contactos: [
+      {
+        nombre: "facebook",
+        url: "https://www.facebook.com/959785477512427/",
+        urlAndroid: "fb://page/959785477512427",
+        urlIOS: "fb://profile/959785477512427"
+      },
+      {
+        nombre: "instagram",
+        url: "https://www.instagram.com/al.toke.foodclub/"
+      },
+      {
+        nombre: "whatsapp",
+        url: "https://wa.me/542657693030"
+      }
+          
+    ]
+  },
+
+];
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+const categorias = document.getElementsByClassName("Category");
+const flyers = document.getElementsByClassName("pictures_gallery");
+
+for (let index = 0; index < categorias.length; index++) {
+  let categoria = categorias[index];
+  categoria.addEventListener("click", () => {
+  let idCategoria = categoria.getAttribute("id");
+  console.log(idCategoria);
+  for (let index = 0; index < flyers.length; index++) {
+    const flyer = flyers[index];
+    flyer.style.display = "none";
+  }
+  for (let index = 0; index < flyers.length; index++) {
+    const flyer = flyers[index];
+    let flyerClassList = flyer.classList;
+    let matchClass = flyerClassList.contains(idCategoria);
+    console.log("   "+ matchClass +" "+ flyer.id);
+    if (matchClass) {
+      flyer.style.display = "block";
+    }
+  }
+  });
+}
+
+for (let index = 0; index < flyers.length; index++) {
+  const flyer = flyers[index];
+  flyer.addEventListener("click", () => {
+    const flyerClassList = flyer.classList;
+    comercios.forEach((comercio) => {
+      const nombreComercio = comercio.nombre;
+      let comercioFlyerMatch = flyerClassList.contains(nombreComercio);
+      if (comercioFlyerMatch) {
+        /*addElement('modal_container', 'img', 'identidadComercio');
+        document.getElementById('identidadComercio')
+                .setAttribute('src', comercio.identidadURL);*/
+        addElement('modal_container', 'div', 'listaRedesSociales', 'ContactoItems');
+        const contactos = comercio.contactos;
+        console.log(contactos);
+        contactos.forEach((contacto) => {
+          const contactoNombre = contacto.nombre;
+          const contactoNombreLinkId = contactoNombre + "Link";
+          const contactoImgId = contactoNombre + "Img";
+          console.log(contactoNombre + " " + contactoNombreLinkId + " " + contactoImgId);
+          addElement('listaRedesSociales', 'a', contactoNombreLinkId, 'RedesLink ContactoItems');
+
+          let urlCompleta = contacto.url;
+
+          if (contactoNombre === "facebook") {
+
+            let isMobile = {
+              Android: function() {
+                  return navigator.userAgent.match(/Android/i);
+              },
+              BlackBerry: function() {
+                  return navigator.userAgent.match(/BlackBerry/i);
+              },
+              iOS: function() {
+                  return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+              },
+              Opera: function() {
+                  return navigator.userAgent.match(/Opera Mini/i);
+              },
+              Windows: function() {
+                  return navigator.userAgent.match(/IEMobile/i);
+              },
+              any: function() {
+                  return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+              }
+            };
+  
+            if (!isMobile.any()) {
+              urlCompleta = contacto.url;
+            }
+      
+            if (isMobile.iOS()) {
+              urlCompleta = contacto.urlIOS;
+            }
+      
+            if (isMobile.Android()) {
+              urlCompleta = contacto.urlAndroid;
+            }
+          }
+
+          document.getElementById(contactoNombreLinkId)
+                  .setAttribute("href", urlCompleta);
+          addElement(contactoNombreLinkId, 'img', contactoImgId, 'contactoItems');
+          contactosIdentidades.forEach((contactoIdentidad) => {
+            const contactoIdentidadNombre = contactoIdentidad.nombre;
+            if (contactoNombre === contactoIdentidadNombre) {
+              const identidadURL = contactoIdentidad.identidadURL;
+              console.log(identidadURL);
+              document.getElementById(contactoImgId)
+                      .setAttribute("src", identidadURL);
+            }
+          });
+        });
+        document.getElementById("modal_background").style.display = "block";
+      }
+    });
+  });
+}
+
+const closeButton = document.getElementById("button_modal");
+closeButton.addEventListener("click", () => {
+  document.getElementById("modal_background").style.display = "none";
+  removeElements("ContactoItems");
+});
+
+function addElement(parentId, elementTag, elementId, elementClassList, html){
+  // Adds an element to the document
+  const p = document.getElementById(parentId);
+  let newElement = document.createElement(elementTag);
+  newElement.setAttribute('id', elementId);
+  newElement.setAttribute('class', elementClassList);
+  //newElement.innerHTML = html;
+  p.appendChild(newElement);
+}
+
+function removeElements(elementsClass) {
+  /*const elementById = document.getElementById(elementId);
+  elementById.parentNode.removeChild(elementById);*/
+  const elementsByClass = document.getElementsByClassName(elementsClass);
+  for (let index = 0; index < elementsByClass.length; index++) {
+    const element = elementsByClass[index];
+    element.parentNode.removeChild(element);
+  }
+}
